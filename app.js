49319b429a60e6b2fd955c6c25a079e531b026d8
ShashankPrajapati13 <shashank1310.prajapati@gmail.com>
2023-07-31 12:11:16 +0530

    link

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://extraordinary-ganache-08360b.netlify.app/',
   credentials:true
 }))
 
