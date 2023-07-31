771d0ac166334dc5bbb33f6293102a1ff37384ac
pawan-s19 <pawans6343@gmail.com>
2023-08-01 01:35:55 +0530

    hosted for server

--- app.js
+++ app.js
@@ -10,20 +10,18 @@ var indexRouter = require("./routes/index");
 
 var app = express();
 const cors = require('cors');
-app.use(cors({
-  origin:'http://localhost:5000', 
-  credentials:true,           
-  optionSuccessStatus:200
-}))
+app.use(cors())
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.use("/", indexRouter);
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
